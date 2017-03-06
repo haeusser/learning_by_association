ab4ea109b10560d74d8f35fb92d9e132203f48a5
Thomas Frerix <thomas.frerix@tum.de>
2017-03-06 17:07:13 +0100

    added USPS dataset tools

---
+++ semisup/usps_tools.py
@@ -0,0 +1,34 @@
+"""
+Download USPS dataset from http://statweb.stanford.edu/~tibs/ElemStatLearn/data.html
+"""
+from __future__ import division
+from __future__ import print_function
+
+import gzip
+import numpy as np
+import data_dirs
+
+DATADIR = data_dirs.usps
+
+
+NUM_LABELS = 10
+IMAGE_SHAPE = [16, 16, 1]
+
+
+def get_data(name):
+    """Utility for convenient data loading."""
+    if name == 'train':
+        return extract_images_labels(DATADIR + '/zip.train.gz')
+    elif name == 'test':
+        return extract_images_labels(DATADIR + '/zip.test.gz')
+
+
+def extract_images_labels(filename):
+    print('Extracting', filename)
+    with gzip.open(filename, 'rb') as f:
+        raw_data = f.read().split()
+    data = np.asarray([raw_data[start:start + 257] for start in range(0, len(raw_data), 257)], dtype=np.float32)
+    images_vec = data[:,1:]
+    images = np.expand_dims(np.reshape(images_vec,(images_vec.shape[0],16,16)), axis=3)
+    labels = data[:,0].astype(int)
+    return images, labels
