f7f500a3cecd1da0c7547d9979634b032a15ca82
Philip Häusser <haeusser@cs.tum.edu>
2017-03-06 15:01:18 +0100

    added public data_dirs template

---
+++ semisup/data_dirs.py
@@ -0,0 +1,12 @@
+"""
+This file contains the directory paths to data sets on your hard drive.
+Change the directories and .gitignore this file.
+"""
+
+synth =             '/path/to/synth/'
+stl10 =             '/path/to/stl10_binary/'
+svhn =              '/path/to/svhn/'
+mnist =             '/path/to/mnist/'
+imagenet =          '/path/to/imagenet/raw-data/'
+imagenet_labels =   '/path/to/imagenet_lsvrc_2015_synsets.txt'
+gtsrb =             '/path/to/gtsrb/'
